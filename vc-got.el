5d2b765c7f68cd6024dd57b0283b97f1c2b0e598
Timo Myyrä <timo.myyra@bittivirhe.fi>
2025-10-17 10:10:54 +0300

    add vc-got--list-revisions
    similar to branch listing, we list last 50 revisions.

--- vc-got.el
+++ vc-got.el
@@ -508,6 +508,20 @@ argument so that must also be given."
           (push (cons (match-string 1) (match-string 2)) alist))
         alist))))
 
+(defun vc-got--list-revisions ()
+  "Return an alist of (branch . commit)."
+  (let (process-file-side-effects)
+    (with-temp-buffer
+      ;; TODO: make number of revisions to query a custom variable
+      (vc-got--log nil 50 nil nil nil nil nil nil t)
+      (let (revisions)
+        (goto-char (point-min))
+        (while (re-search-forward "\\(^[0-9-]\\{10\\}\\) \\([a-z0-9]+\\) \\(.*\\)$"
+                                  nil t)
+          (push (list (match-string 1) (match-string 2) (match-string 3))
+                revisions))
+        revisions))))
+
 (defun vc-got--prompt-branch (message)
   "Prompt user for a branch.  The prompt displays MESSAGE for the user."
   ;; TODO: sort branches
